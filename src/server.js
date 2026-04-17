218f1cc646573456b75c30bda136b3edd0f12672
RadhapyariDevi <radhapyarikhuman123@gmail.com>
2026-04-17 19:59:11 +0530

    initial setup

---
+++ src/server.js
@@ -0,0 +1,50 @@
+import express from "express";
+import {config} from 'dotenv'
+import {connectDB, disconnectDB} from "./config/db.js"
+
+config();
+connectDB();
+
+const app = express();
+
+app.use(express.json());
+
+
+
+app.get("/", (req, res) => {
+  res.send(" API running...");
+});
+
+const PORT = 5000;
+
+
+const server = app.listen(PORT, ()=>{
+    console.log(`Server running on PORT ${PORT}`);
+});
+
+process.on("unhandledRejection", (err) => {
+    console.error("Unhandled Rejection: ", err);
+    server.close(async () => {
+        await disconnectDB();
+        process.exit(1);
+    });
+});
+
+process.on("uncaughtException", async (err) => {
+    console.error("Uncaught Exception: ", err);
+    server.close(async () => {
+        await disconnectDB();
+        process.exit(1);
+    });
+});
+
+process.on("SIGTERM",async  () => {
+    console.error("SIGTERM received, shutting down gracefully");
+    server.close(async () => {
+        await disconnectDB();
+        process.exit(0);
+    });
+});
+
+
+
